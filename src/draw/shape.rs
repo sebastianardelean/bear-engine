6f68b5e50269bba789248de8cb3ca9a79b460af6
Sebastian Ardelean <ardeleansm@gmail.com>
2025-11-30 18:04:20 +0200

    fixed bug in shape

--- src/draw/shape.rs
+++ src/draw/shape.rs
@@ -2,40 +2,31 @@ use gl::types::{GLsizei, GLuint};
 use std::os::raw::c_void;
 use std::sync::OnceLock;
 
-
-
-#[derive(Clone,Copy)]
+#[derive(Clone, Copy)]
 pub enum DrawMode {
     RenderTexture,
     RenderLight,
-    RenderBoth
+    RenderBoth,
 }
-
 
 pub struct Shape {
     vertices: Vec<f32>,
     indices: Option<Vec<u32>>,
     draw_mode: DrawMode,
     gpu_buffers: OnceLock<(u32, u32, u32)>,
-    number_of_triangles:i32
+    number_of_triangles: i32,
 }
 const COORDINATES_INDEX: u32 = 0;
 const COORDINATES_SIZE: i32 = 3;
 const TEXTURE_SIZE: i32 = 2;
 const NORMAL_SIZE: i32 = 3;
 impl Shape {
-    pub fn new(vertices: Vec<f32>, indices: Option<Vec<u32>>,draw_mode: DrawMode) -> Self {
-        let len:i32 = vertices.len() as i32;
-        let number_of_triangles:i32 = match draw_mode{
-            DrawMode::RenderTexture=>{
-                len/(COORDINATES_SIZE+TEXTURE_SIZE)
-            },
-            DrawMode::RenderLight =>{
-                len/(COORDINATES_SIZE+NORMAL_SIZE)
-            },
-            DrawMode::RenderBoth=>{
-                len/(COORDINATES_SIZE+TEXTURE_SIZE+NORMAL_SIZE)
-            }
+    pub fn new(vertices: Vec<f32>, indices: Option<Vec<u32>>, draw_mode: DrawMode) -> Self {
+        let len: i32 = vertices.len() as i32;
+        let number_of_triangles: i32 = match draw_mode {
+            DrawMode::RenderTexture => len / (COORDINATES_SIZE + TEXTURE_SIZE),
+            DrawMode::RenderLight => len / (COORDINATES_SIZE + NORMAL_SIZE),
+            DrawMode::RenderBoth => len / (COORDINATES_SIZE + TEXTURE_SIZE + NORMAL_SIZE),
         };
 
         return Shape {
@@ -43,15 +34,11 @@ impl Shape {
             indices: indices,
             draw_mode: draw_mode,
             gpu_buffers: OnceLock::new(),
-            number_of_triangles: number_of_triangles
+            number_of_triangles: number_of_triangles,
         };
     }
 
-
-
-    fn init_gpu_buffers(
-        &self,
-    ) -> (GLuint, GLuint, GLuint) {
+    fn init_gpu_buffers(&self) -> (GLuint, GLuint, GLuint) {
         let mut vao: GLuint = 0;
         let mut vbo: GLuint = 0;
         let mut ebo: GLuint = 0;
@@ -62,7 +49,6 @@ impl Shape {
             if self.indices.is_some() {
                 gl::GenBuffers(1, &mut ebo);
             }
-
 
             gl::BindVertexArray(vao);
 
@@ -84,8 +70,8 @@ impl Shape {
             }
             match self.draw_mode {
                 DrawMode::RenderTexture => {
-                    let stride:usize = (COORDINATES_SIZE+TEXTURE_SIZE) as usize;
-                    let texture_offset:usize = COORDINATES_SIZE as usize;
+                    let stride: usize = (COORDINATES_SIZE + TEXTURE_SIZE) as usize;
+                    let texture_offset: usize = COORDINATES_SIZE as usize;
                     // position attribute
                     gl::VertexAttribPointer(
                         0,
@@ -107,10 +93,10 @@ impl Shape {
                         (texture_offset * std::mem::size_of::<f32>()) as *const c_void,
                     );
                     gl::EnableVertexAttribArray(1);
-                },
+                }
                 DrawMode::RenderLight => {
-                    let stride:usize = (COORDINATES_SIZE+NORMAL_SIZE) as usize;
-                    let normal_offset:usize = COORDINATES_SIZE as usize;
+                    let stride: usize = (COORDINATES_SIZE + NORMAL_SIZE) as usize;
+                    let normal_offset: usize = COORDINATES_SIZE as usize;
                     // position attribute
                     gl::VertexAttribPointer(
                         0,
@@ -133,10 +119,9 @@ impl Shape {
                     gl::EnableVertexAttribArray(1);
                 }
                 DrawMode::RenderBoth => {
-                    let stride:usize = (COORDINATES_SIZE+TEXTURE_SIZE+NORMAL_SIZE) as usize;
-                    let texture_offset:usize = COORDINATES_SIZE as usize;
-                    let normal_offset:usize = (COORDINATES_SIZE+TEXTURE_SIZE) as usize;
-
+                    let stride: usize = (COORDINATES_SIZE + TEXTURE_SIZE + NORMAL_SIZE) as usize;
+                    let texture_offset: usize = (COORDINATES_SIZE + TEXTURE_SIZE) as usize;
+                    let normal_offset: usize = (COORDINATES_SIZE) as usize;
 
                     // position attribute
                     gl::VertexAttribPointer(
@@ -148,20 +133,9 @@ impl Shape {
                         std::ptr::null(),
                     );
                     gl::EnableVertexAttribArray(0);
-
-                    // texture attribute
-                    gl::VertexAttribPointer(
-                        1,
-                        TEXTURE_SIZE,
-                        gl::FLOAT,
-                        gl::FALSE,
-                        (stride * std::mem::size_of::<f32>()) as GLsizei,
-                        (texture_offset * std::mem::size_of::<f32>()) as *const c_void,
-                    );
-                    gl::EnableVertexAttribArray(1);
                     // light and material attribute
                     gl::VertexAttribPointer(
-                        2,
+                        1,
                         NORMAL_SIZE,
                         gl::FLOAT,
                         gl::FALSE,
@@ -169,19 +143,27 @@ impl Shape {
                         (normal_offset * std::mem::size_of::<f32>()) as *const c_void,
                     );
                     gl::EnableVertexAttribArray(1);
+                    // texture attribute
+                    gl::VertexAttribPointer(
+                        2,
+                        TEXTURE_SIZE,
+                        gl::FLOAT,
+                        gl::FALSE,
+                        (stride * std::mem::size_of::<f32>()) as GLsizei,
+                        (texture_offset * std::mem::size_of::<f32>()) as *const c_void,
+                    );
+                    gl::EnableVertexAttribArray(2);
                 }
             }
-
         }
 
         return (vao, vbo, ebo);
     }
 
     pub fn draw(&mut self) {
-        let (vao, _vbo, _ebo): (GLuint, GLuint, GLuint) = *self
-            .gpu_buffers
-            .get_or_init(|| self.init_gpu_buffers());
-        if let Some(ref indices) = self.indices{
+        let (vao, _vbo, _ebo): (GLuint, GLuint, GLuint) =
+            *self.gpu_buffers.get_or_init(|| self.init_gpu_buffers());
+        if let Some(ref indices) = self.indices {
             unsafe {
                 gl::BindVertexArray(vao);
                 gl::DrawElements(
